5900cb3af2511ca3d639a46ebd10bee95be06bef
Paweł <pawel_gnat@o2.pl>
2023-12-09 14:53:22 +0100

    day 9

--- tasks/2023-12-09/index.ts
+++ tasks/2023-12-09/index.ts
@@ -1,1 +1,35 @@
-// Tutaj skopiuj kod zadania
+export interface Tool {
+	init: () => void
+	update: () => void
+	dispose: () => void
+}
+
+interface EquipmentType {
+	tools: Tool[]
+	initialized: boolean
+}
+
+export class Equipment {
+	equipment: EquipmentType = { tools: [], initialized: false }
+
+	registerTools(tool: Tool) {
+		this.equipment.tools.push(tool)
+	}
+
+	initializeTools() {
+		this.equipment.tools.forEach(eq => eq.init())
+		this.equipment.initialized = true
+	}
+
+	updateTools() {
+		if (!this.equipment.initialized) {
+			throw new Error('Cannot update any tools before initialization.')
+		}
+
+		this.equipment.tools.forEach(eq => eq.update())
+	}
+
+	disposeTools() {
+		this.equipment.tools.forEach(eq => eq.dispose())
+	}
+}
